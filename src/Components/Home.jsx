ac1783998f271209cefbd285580ddaff5f317d36
unknown <abdulbatinchy@gmail.com>
2024-02-22 00:19:56 +0600

    about us and trending section done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,15 @@
+import About from "./About";
 import Category from "./Category";
 import Hero from "./Hero";
+import Trending from "./Trending";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Category />
+      <Trending></Trending>
+      <About></About>
     </div>
   );
 };
